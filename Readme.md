af6c4796bc68d7a353d6e07c8f4fe929aae0fd06
uche-hub <ucj.theuche@gmail.com>
2024-12-17 21:04:10 +0100

    First Commit

---
+++ Readme.md
@@ -0,0 +1,2 @@
+main.dart
+Project 1
---
+++ main.dart
@@ -0,0 +1,18 @@
+// A Function
+void main() {
+  // Variable
+
+  /* To Create a variable:
+  <datatype> <variablename> = value;
+   */
+
+  int firstvalue = 400;
+  int secondvalue = 50;
+
+  print(firstvalue);
+  print(secondvalue);
+
+  print(firstvalue + secondvalue);
+  print(firstvalue - secondvalue);
+  print(firstvalue * secondvalue);
+}
