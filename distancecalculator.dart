1dca07f4f534826c3d1843a7295f76b9b7d94576
uche-hub <ucj.theuche@gmail.com>
2024-12-19 22:32:16 +0100

    Shipping Distance Calculator

---
+++ distancecalculator.dart
@@ -0,0 +1,27 @@
+void main() {
+  String destinationZone = "XYZ";
+  double weightInKg = 6;
+
+  // Using If Statement
+  if (destinationZone == "PQR") {
+    print('Shipping Cost is ${weightInKg * 10}');
+  } else if (destinationZone == "XYZ") {
+    print('Shipping Cost is ${weightInKg * 5}');
+  } else if (destinationZone == "ABC") {
+    print('Shipping Cost is ${weightInKg * 7}');
+  } else {
+    print("Shipping Unavaliable");
+  }
+
+  // Using Switch Statement
+  switch (destinationZone) {
+    case "PQR":
+      print('Shipping Cost is ${weightInKg * 10}');
+    case "XYZ":
+      print ('Shipping Cost is ${weightInKg * 5}');
+    case "ABC":
+      print('Shipping Cost is ${weightInKg * 7}');
+    default:
+      print("Shipping Unavailable");
+  }
+}
