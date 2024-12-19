7708eff28511a82b66850c8a7519dcd3fc549a87
uche-hub <ucj.theuche@gmail.com>
2024-12-19 21:32:29 +0100

    ! with boolean

--- ifStatement.dart
+++ ifStatement.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: file_names
-
+bool isAllowed = false;
 void main() {
   // if statements
 
-  int age = 24;
+  int age = 18;
 
   // if Statement saying that if 24 is greater or equal to 30 then say "Early 30s" if its not then say  "Early 20s";
   if (age >= 30) {
@@ -24,5 +24,20 @@ void main() {
     print("24 Years Old");
   } else {
     print("Early 20s");
+  }
+
+  print("========= (!) with boolean =========");
+  /* 
+    Use the (!) with boolean
+
+    variable is located outside the function
+  */
+
+  // ! can also be written as isAllowed != true
+  if (age != 18 && !isAllowed) {
+    // ! mean is not or false.
+    print("No");
+  } else {
+    print("Yes");
   }
 }
