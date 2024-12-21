d78ca2171175502c9ac1453fc7bd8422c8c6aa23
uche-hub <ucj.theuche@gmail.com>
2024-12-21 11:17:02 +0100

    Understanding Functions

--- funtions.dart
+++ funtions.dart
@@ -1,3 +1,34 @@
 void main() {
-  
+  // Functions
+
+  /* They are block of codes that contains your code */
+  print(printName());
+  ucheName();
+
+  // to assign the toAssign  Function
+  int age = toAssign();
+  print(age);
 }
+
+/* So what this is doing is that..... when you create a function and add your code you need to call
+  it in the void main()  for it to run.
+*/
+
+String printName() {
+  return 'Uchenna';
+}
+
+void ucheName() {
+  print("Uchenna");
+}
+
+int toAssign() {
+  return 24;
+}
+
+// void means that the function is not returning anything
+
+/*
+  <datatype> fnName() {
+  }
+*/
