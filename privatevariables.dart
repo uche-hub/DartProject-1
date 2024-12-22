b8ef6be34c4a969fc0f4922480e1738e7a4deb21
uche-hub <ucj.theuche@gmail.com>
2024-12-22 21:02:48 +0100

    Private Variables

--- privatevariables.dart
+++ privatevariables.dart
@@ -1,3 +1,29 @@
 void main() {
-  
+  final cookie = Cookie(shape: "Uchenna Ndukwe", size: 24);
+
+  print(cookie.shape);
+}
+
+// Private variables should be accessible
+
+class Cookie {
+  final String shape;
+  final double size;
+
+  Cookie({required this.shape, required this.size}) {
+    baking();
+  }
+
+  //private variable
+  int _height = 4;
+  int _width = 5;
+
+  // Method
+  int calculateSize() {
+    return _height * _width;
+  }
+
+  void baking() {
+    print("Baking has started");
+  }
 }
