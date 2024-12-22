5deb493bae04412a44ea06b2fae28064eaa420bc
uche-hub <ucj.theuche@gmail.com>
2024-12-22 22:40:59 +0100

    Static Variable

--- staticfunctionandvariables.dart
+++ staticfunctionandvariables.dart
@@ -1,4 +1,12 @@
 void main() {
-
+  print(Constants.greeting);
+  print(Constants.bye);
 }
 
+class Constants {
+  // Static helps saves memory
+
+  // Static Variable
+  static String greeting = "Hello how are you?";
+  static String bye = "Bye";
+}
