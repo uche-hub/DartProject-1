39213a44897582dc6e548d6912409a97e87e5a3c
uche-hub <ucj.theuche@gmail.com>
2024-12-22 21:11:45 +0100

    Getters and Setters

--- privatevariables.dart
+++ privatevariables.dart
@@ -2,6 +2,12 @@ void main() {
   final cookie = Cookie(shape: "Uchenna Ndukwe", size: 24);
 
   print(cookie.shape);
+
+  print(cookie.height);
+
+  cookie.setHeight = 15;
+
+  print(cookie.height);
 }
 
 // Private variables should be accessible
@@ -18,7 +24,21 @@ class Cookie {
   int _height = 4;
   int _width = 5;
 
+  // Getters
+  int get height => _height;
+
+  // Setters
+  set setHeight(int h) {
+    _height = h;
+  }
+
   // Method
+
+  // Method for the Getters
+  void modifyHeight(int h) {
+    _height = h;
+  }
+
   int calculateSize() {
     return _height * _width;
   }
