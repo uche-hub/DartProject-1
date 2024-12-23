2c79a9a60a02f0084532445d379d2c5072ffdce7
uche-hub <ucj.theuche@gmail.com>
2024-12-23 11:48:30 +0100

    Mixin

--- mixins.dart
+++ mixins.dart
@@ -1,3 +1,16 @@
 void main() {
+  // Mixin mixes alll the properties in a class
+  final anim = Animal();
+  anim.fn();
+}
+
+mixin Jump {
+  int jumping = 10;
+}
+
+class Animal with Jump {
   
+  void fn() {
+    print(jumping);
+  }
 }
