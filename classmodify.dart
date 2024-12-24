05f3b09f1f73574ec7560e99a2f22a0d4a0037f8
uche-hub <ucj.theuche@gmail.com>
2024-12-24 09:31:09 +0100

    More Modifiers

--- classmodify.dart
+++ classmodify.dart
@@ -9,9 +9,26 @@ void main() {
     case Human():
       print("Human");
   }
-}
 
+  /* =========== Difference bewteen Final and sealed class =========== */
+  // sealed class cannot be constructed 
+  // final class can be constructed
+  // in the final class we dont get the features the sealed class give us 
+} 
+
+/* ============ List of Class Modifiers =========== */
+// 1. sealed class
+// 2. final class
+// 3. base class
+// 4. interface class
+// 5. mixin class
+
+// sealed prevent other class from implementing or extending it
 sealed class Animal {}
+
+//finsal Modifier
+// final cannot be implemented or extended outside of the same libary
+final class Animal1 {}
 
 class Human implements Animal {}
 
