c629026df69fc7b989c0c32e46a1069db3bfad3a
uche-hub <ucj.theuche@gmail.com>
2024-12-28 01:21:45 +0100

    Worked on how to handle exception using the try catch block

--- exceptionhandling.dart
+++ exceptionhandling.dart
@@ -1,3 +1,19 @@
 void main() {
-  
+  // Exception Handling - exceptions are being handled
+  // the ~ help handle the exceptions, instead of giving us the 3.3333 it converts it to a single int
+  print(10 ~/ 3);
+  print(10 / 0);
+
+  // this will throw an exception
+  // so handle exceptionwe will use the try and catch block.
+
+  try {
+    // in this try you put in codes that you know can throw an exception
+    print(10 ~/ 0);
+  } catch (e) {
+    // (e) mean exception
+    print(e);
+  }
+
+  print("Uchenna");
 }
