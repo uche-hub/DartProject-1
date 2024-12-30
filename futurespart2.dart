b6eb38f24bd864f84888582182c908dcd4164587
uche-hub <ucj.theuche@gmail.com>
2024-12-30 14:47:29 +0100

    Created a yaml file and called the api the lists the names

--- futurespart2.dart
+++ futurespart2.dart
@@ -1,3 +1,9 @@
-void main() {
-  
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', 'users');
+  final res = await http.get(url);
+
+  print(jsonEncode(res.body));
 }
