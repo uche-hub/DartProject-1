934646b43d3bd7984702c497ef196904f4b6be2b
uche-hub <ucj.theuche@gmail.com>
2024-12-31 00:35:45 +0100

    created a count down program

--- streams.dart
+++ streams.dart
@@ -1,6 +1,22 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
 
 void main() {
+  // Streams - it is an async generator, which allows us to provide a syn of values
+  // the .listen() give us a stream subscription
+  countDown().listen((val) {
+    print(val);
+  }, onDone: () {
+    print("Count Down Completed!!!");
+  });
+}
 
+Stream<int> countDown() async* {
+  for (int i = 5; i > 0; i--) {
+    // yield means to provide in place of "reture"
+    yield i;
+
+    // to let it count down after 1 seconds
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
